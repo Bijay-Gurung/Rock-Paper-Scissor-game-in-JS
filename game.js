f53421c7b9dcbff08dda64a59dba48565ffd399d
Bijay-Gurung <stha4580@gmail.com>
2023-05-31 09:30:48 +0545

    My First Web Game

---
+++ game.js
@@ -0,0 +1,18 @@
+// Toggle Button
+function myFunction(){
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+
+    const button = document.querySelector("button");
+    if (element.classList.contains("dark-mode")){
+        button.textContent="Light";
+    }
+    else{
+        button.textContent="Dark";
+    }
+}
+
+function playGame() {
+    window.open("game.html","_blank");
+}
+  
